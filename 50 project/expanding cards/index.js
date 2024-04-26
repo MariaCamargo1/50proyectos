b4bbf571a519e84813221d7e7205140194ca58b5
maria camargo <maria.camargo@globant.com>
2024-04-26 09:46:16 -0500

    actualizacion de carpeta

--- 50 project/expanding cards/index.js
+++
@@ -1,9 +0,0 @@
-const catCards = document.querySelectorAll('.cat-card');
-catCards.forEach(card => {
-    card.addEventListener('click', () => {
-        catCards.forEach(card => {
-            card.classList.remove('active');
-        });
-        card.classList.add('active');
-    });
-});
